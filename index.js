6c2d0d31274931be1fb0feca22f5722b448f5bea
Stepdab <SKYBINPLAY@gmail.com>
2024-01-06 12:03:31 +0300

    v3

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import {json} from 'body-parser'
 
 let server = express()
 
+server.use('/', json({limit: '3mb'}))
+
 server.get('/', (req,res) => {
     res.send({success: true, result: Math.random()})
+})
+
+server.post('/background/items', (req, res) => {
+    let {items} = req.body
+
+    console.log(items)
+
+    res.send({success: true})
 })
 
 server.listen(3000)
